d4e8972889e8de5c0badcc0fe81501a1ad22b6cf
Anmol Heena <anmol.heena@appinventiv.com>
2018-04-06 16:43:47 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gradient
+//
+//  Created by Appinventiv-PC on 28/03/18.
+//  Copyright © 2018 Appinventiv-PC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ gradient.swift
@@ -0,0 +1,12 @@
+//
+//  gradient.swift
+//  Gradient
+//
+//  Created by Appinventiv-PC on 28/03/18.
+//  Copyright © 2018 Appinventiv-PC. All rights reserved.
+//
+
+import Foundation
+class GradientView : UIView{
+    @
+}
